12180ac97173aed7aaf4ec5e401b9cd390590642
Bernardo Viero <61853561+bernardodsv@users.noreply.github.com>
2020-06-23 16:28:54 -0300

    Create Vetores.c

---
+++ 05_Vetores/Vetores.c
@@ -0,0 +1,26 @@
+/*
+
+Vetores, são utilizados para ajudar no código, sempre facilitando e simplificando.
+
+Estrutura:
+
+  int notas[5]; //falando quantas notas são.
+
+  for( i = 0 ; i <= 4; i++) //Usando a estrutura de repetição para ajudar.
+  {
+    printf("Digite as nota%d\n",i); // Podendo mostrar ou pedir as notas.
+    scanf("%d",nota[5]);
+  }
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+
+
+
+    return 0;
+}
