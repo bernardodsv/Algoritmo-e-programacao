b9dab77a578f36e2c99d23a71c6f75be5bc8d277
Bernardo Viero <61853561+bernardodsv@users.noreply.github.com>
2020-06-23 16:28:23 -0300

    Create IF.c

---
+++ 03_Condicionais/IF/IF.c
@@ -0,0 +1,28 @@
+/*
+   EXEMPLO DE USO: CONTROLE CONDICIONAL com o comando if
+   Algoritmo com a implementação de um exemplo em que deverá ser
+   solicitado, ao usuário, qual a temperatura corporal de uma pessoa.
+   Com base na resposta informada, o algoritmo oferece uma ou outra resposta.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	// início do algoritmo
+	float temperatura;
+	printf("Exemplo de CONTROLE CONDICIONAL com o comando if\n");
+	printf("Informe o valor para a temperatura corporal:\n");
+	scanf("%f", &temperatura);
+
+	// Nesse ponto há o teste para o controle condicional
+	if (temperatura >= 39) {
+		printf("Pessoa com febre alta\n");
+	}
+
+	// continuação do algoritmo
+	printf("Fluxo do programa segue normalmente...\n");
+	printf("...caso existam mais comandos a serem usados no algoritmo\n");
+
+	return 0;
+}
