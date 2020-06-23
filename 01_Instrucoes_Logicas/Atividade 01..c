ad862e4d4c4228a20ddd3ff2f6e42724e80ccf04
Bernardo Viero <61853561+bernardodsv@users.noreply.github.com>
2020-06-23 16:27:24 -0300

    Create Atividade 01..c

---
+++ 01_Instrucoes_Logicas/Atividade 01..c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+int main(){
+
+    setlocale(LC_ALL, "Portuguese");
+
+    printf("\t___________________________________________________________________________________\n");
+    printf("\t|                              MENU DO PAINEL                                     |\n");
+    printf("\t|                                                                                 |\n");
+    printf("\t|    *************                                     *************              |\n");
+    printf("\t|    *           *                09:17                *           *              |\n");
+    printf("\t|    *    GPS    *              05/03/2020             *  Músicas  *              |\n");
+    printf("\t|    *           *                                     *           *              |\n");
+    printf("\t|    *************                                     *************              |\n");
+    printf("\t|                                                                                 |\n");
+    printf("\t|                                                                                 |\n");
+    printf("\t|    *************          *************              *************              |\n");
+    printf("\t|    *           *          *           *              *           *              |\n");
+    printf("\t|    *  Vídeos   *          * Pendrive  *              *   Rádio   *              |\n");
+    printf("\t|    *           *          *           *              *           *              |\n");
+    printf("\t|    *************          *************              *************              |\n");
+    printf("\t|                                                                                 |\n");
+    printf("\t-----------------------------------------------------------------------------------\n");
+    system("pause");
+    return 0;
+}
